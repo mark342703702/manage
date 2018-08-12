cf295aacdd57bba6bbac6e92c51a4302d017d2ad
黄海 <342703702@qq.com>
2018-08-12 11:15:17 +0800

    2018812

--- src/routes/Admin/addAdmin.js
+++ src/routes/Admin/addAdmin.js
@@ -1,5 +1,11 @@
 import React from 'react';
-import { connect } from 'dva';
+// import { connect } from 'dva';
+import { Link } from 'dva/router';
+import PageHeader from '../../components/PageHeader';
+import { Card } from 'antd';
+import styles from './addAdmin.less';
+
+
 
 export default class Test extends React.Component {
     state = {
@@ -7,9 +13,25 @@ export default class Test extends React.Component {
     }
 
     render(){
+        const breadcrumbList = [{
+            title: '首页',
+            href: '/',
+        }, {
+            title: '添加管理员'
+        }];
+
         return(
-            <div style={{textAlign:'center'}}>
-                添加管理员
+            <div className={styles.layout}>
+               <PageHeader
+                className="tabs"
+                title={<div className="title">添加管理员</div>}
+                content={<div className="content">添加管理员页面用于收集新增管理员信息，系统生成一个新的管理员用户</div>}
+                breadcrumbList={breadcrumbList}
+                linkElement	= {Link}
+                />
+                <Card bordered={false} className={styles.form}>
+                    
+                </Card>
             </div>
         )
     }
