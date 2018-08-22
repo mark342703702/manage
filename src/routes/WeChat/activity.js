9c1aa1ce42a14cb09e4db27f5d27f44bce095155
mark342703702 <you@example.com>
2018-08-22 17:18:42 +0800

    2018822

--- src/routes/WeChat/activity.js
+++ src/routes/WeChat/activity.js
@@ -1,10 +1,31 @@
 import React from 'react';
-
+import PageHeader from '../../components/PageHeader';
+import { Link } from 'dva/router';
+import styles from './activity.less';
+import ListBar from './components/_listBar';
+import { Card } from 'antd';
 export default class Activity extends React.Component {
     render(){
+
+        const breadcrumbList = [{
+            title: '首页',
+            href: '/',
+        }, {
+            title: '活动管理'
+        }];
+
         return(
-            <div style={{textAlign:'center'}}>
-                weixin
+            <div className={styles.layout}>
+                <PageHeader
+                className="tabs"
+                title={<div className="title">活动管理</div>}
+                content={<div className="content">活动管理员页面用于查看当前存在的活动，新增自定义活动</div>}
+                breadcrumbList={breadcrumbList}
+                linkElement	= {Link}
+                />
+                <div className={styles.content}>
+                    <ListBar />
+                </div>
             </div>
         )
     }
---
+++ src/routes/WeChat/components/_listBar.js
@@ -0,0 +1,42 @@
+import React from "react";
+import styles from './_listBar.less';
+import { Card , Icon, Button} from 'antd';
+const { Meta } = Card;
+export default class ListBar extends React.Component {
+
+    render(){
+        return (
+            <div className={styles.layout}>
+                <div className={`${styles.item} ${styles.add}`}>
+                  <Button type="dashed" className={styles.newButton}>
+                        <Icon type="plus" /> 新增产品
+                  </Button>
+                </div>
+                <div className={styles.item}>
+                    <Card 
+                    cover={<img alt="example" src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png" height="145"/>}
+                    actions={[<Icon type="setting" />, <Icon type="edit" />, <Icon type="ellipsis" />]}
+                    style={{ width: 240 }} bordered={true} hoverable>
+                        <Meta
+                        title="Europe Street beat"
+                        description="www.instagram.com"
+                        />
+                    </Card>
+                </div>
+                <div className={styles.item}>
+                    <Card 
+                    cover={<img alt="example" src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png" height="145"/>}
+                    actions={[<Icon type="setting" />, <Icon type="edit" />, <Icon type="ellipsis" />]}
+                    style={{ width: 240 }} bordered={true} hoverable>
+                        <Meta
+                        title="Europe Street beat"
+                        description="www.instagram.com"
+                        />
+                    </Card>
+                </div>
+                
+            </div>
+        )
+    }
+}
+
